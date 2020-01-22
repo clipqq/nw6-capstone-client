0fa4c662e87548a92bb3f4e0a6a31cc26788a454
Clifford Tran <clipqq>
2020-01-21 22:26:21 -0800

    installed jwt-decoder

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World! Welcome to WTDD!
         </a>
       </header>
     </div>
