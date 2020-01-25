5f50aa3ba51ff15c1cbc3d4a3e6f5df51844bb50
Anthony Gelder <anthony.gelder@gmail.com>
2020-01-24 20:37:42 -0800

    header and landing

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import { Link }from 'react-router-dom'
+import './Header.css'
 
 class Header extends Component {
 
     render() {
 
         return (
-            <>        
-                <h1>Header</h1>
+            <>     
+                <Link to={'/'} className="logo" >  
+                    <h1>WTDD</h1>
+                </Link> 
+                <div className="buttons">
+                    <Link to={'/login'} >
+                        <button>
+                            Log in
+                        </button>
+                    </Link>
+                    <Link to={'/register'} >
+                        <button>
+                            Register
+                        </button>
+                    </Link>
+                </div>
             </>
         );
     }
