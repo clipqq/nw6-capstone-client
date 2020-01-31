ce8e589485d598d30bd43f309ff2e1adeb544991
Queen Shabazz <queennefertitishabazz@gmail.com>
2020-01-30 22:10:47 -0800

    event login+register

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,35 +1,89 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './Login.css'
+import config from '../config'
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: '',
-            password: ''
+            user_email: '',
+            password: '',
+            user_name:'',
+            error: ''
+
         }
+        
+        this.login=this.login.bind(this)
     }
 
-    updateEmail(email) {
-        this.setState({email: email});
+    updateName(user_name) {
+        this.setState({user_name: user_name});
+    }
+
+    updateEmail(user_email) {
+        this.setState({user_email: user_email});
     }
     updatePassword(password) {
         this.setState({password: password});
     }
+<<<<<<< HEAD
+    login(e){
+        console.log(this.state.user_name,'USERNAME')
+        e.preventDefault();
+        if(this.state.email==='' || this.state.password===''){
+            this.setState({error: "Please add password and email"})
+            return
+        }
+        const headers = new Headers ();
+        headers.append('Content-Type', 'application/json');
+        const options = {
+            method: 'POST',
+            headers,
+            body: JSON.stringify({
+                user_name:this.state.user_name,
+                user_email:this.state.user_email,
+                password: this.state.password,
+            }),
+        };
+        const request = new Request (`${config.API_ENDPOINT}/auth/login`, options)
+        fetch(request)
+        .then(res=>{
+            if(!res.ok){
+                throw res
+            }
+            return res.json()
+        })
+        .then(data => { 
+
+            localStorage.setItem("authToken", data.authToken)
+            this.props.history.push('/saved')
+         })
+         .catch(err => {
+            if(err.status===400){
+                this.setState({error: "Incorrect username or password"})
+            }
+            })
+
+    }
+
+=======
     
+>>>>>>> 2587543dbbf7368ee6d332e020695654120da4ff
 
     render() {
         return (
             <div className="login">        
                 <h1>Login</h1>
                 <div className="form-group">
+                <label htmlFor="email">UserName:</label>
+                    <input required type="text" name="user_name" id="user_name" value={ this.state.user_name } onChange={e => this.updateName(e.target.value)}/>
                     <label htmlFor="email">Email:</label>
-                    <input required type="email" name="email" id="email" value={ this.state.email } onChange={e => this.updateEmail(e.target.value)}/>
+                    <input required type="email" name="email" id="email" value={ this.state.user_email } onChange={e => this.updateEmail(e.target.value)}/>
                     <label htmlFor="password">Password:</label>
                     <input required type="text" name="password" id="password" value={ this.state.password } onChange={e => this.updatePassword(e.target.value)}/>
                     <div className="buttons">
-                        <button type="submit">
+                        <button  onClick={this.login}type="submit">
                             Login
                         </button>
                         <Link to='/'>
--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import config from '../config'
 
 class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: '',
+            user_email: '',
             name: '',
             password: '',
+            user_name:'',
             confirm_password: ''
         }
+        // this.updateEmail=this.updateEmail.bind(this)
+        // this.updateName=this.updateName.bind(this)
+        // this.updatePassword=this.updatePassword.bind(this)
+        // this.confirmPassword=this.confirmPassword.bind(this)
     }
 
-    updateEmail(email) {
-        this.setState({email: email});
+    updateEmail(user_email) {
+        this.setState({user_email: user_email});
     }
-    updateName(name) {
-        this.setState({name: name});
+    updateName(user_name) {
+        this.setState({user_name: user_name});
     }
     updatePassword(password) {
         this.setState({password: password});
@@ -24,22 +30,55 @@ class Register extends Component {
     updateConfirmPassword(confirm_password) {
         this.setState({confirm_password: confirm_password});
     }
-
+    addUser= (e) =>{
+        e.preventDefault();
+        if(this.state.user_email==='' || this.state.password===''){
+            this.setState({error: "Please add password and user_email"})
+            return
+        }
+        const headers = new Headers ();
+        headers.append('Content-Type', 'application/json');
+        const options = {
+            method: 'POST',
+            headers,
+            body: JSON.stringify({
+                user_name: this.state.user_name,
+                user_email: this.state.user_email,
+                password: this.state.password,
+            }),
+        };
+        const request = new Request (`${config.API_ENDPOINT}/user`, options)
+        fetch(request)
+        .then(res=>{
+            if(!res.ok){
+                throw res
+            }
+            return res.json()
+        })
+        .then(data=>
+            this.props.history.push("/login")
+        )
+        .catch(err => {
+            if(err.status===400){
+                this.setState({error: "email already taken"})
+            }
+            })
+    }
     render() {
         return (
             <div className="login">        
                 <h1>Register</h1>
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
-                    <input required type="email" name="email" id="email" value={ this.state.email } onChange={e => this.updateEmail(e.target.value)}/>
+                    <input required type="email" name="email" id="email" value={ this.state.user_email } onChange={e => this.updateEmail(e.target.value)}/>
                     <label htmlFor="name">Name:</label>
-                    <input required type="name" name="name" id="name" value={ this.state.name } onChange={e => this.updateName(e.target.value)}/>
+                    <input required type="name" name="name" id="name" value={ this.state.user_name } onChange={e => this.updateName(e.target.value)}/>
                     <label htmlFor="password">Password:</label>
                     <input required type="text" name="password" id="password" value={ this.state.password } onChange={e => this.updatePassword(e.target.value)}/>
                     <label htmlFor="confirm_password">Confirm Password:</label>
                     <input required type="text" name="confirm_password" id="confirm_password" value={ this.state.confirm_password } onChange={e => this.updateConfirmPassword(e.target.value)}/>
                     <div className="buttons">
-                        <button type="submit">
+                        <button type="submit" onClick={this.addUser}>
                             Register
                         </button>
                         <Link to='/'>
