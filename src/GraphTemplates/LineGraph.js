02a9f83f7716c1c102cc1bf00720e8cb031d9a3d
clipqq <clifford.l.tran@gmail.com>
2020-02-04 18:36:49 -0800

    GET currently in Graph

--- src/GraphTemplates/LineGraph.js
+++ src/GraphTemplates/LineGraph.js
@@ -1,38 +1,65 @@
-import React from 'react';
-import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries} from 'react-vis';
+import React from 'react'
+import {
+    XYPlot,
+    XAxis,
+    YAxis,
+    VerticalGridLines,
+    HorizontalGridLines,
+    LineSeries,
+} from 'react-vis'
 
-const LineGraph = (props) => {
+export default class LineGraph extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dataArr: this.props.data,
+        }
 
-    const dataArr = props.data.map((d)=> {
-        return {x: d.year + '/' + d.quarter, 
-        y: parseFloat(d.count/1000)}
-    });
+        console.log('inside LineGraph', this.props)
+    }
+    // const dataArr = props.data.map((d)=> {
+    //     return {x: d.year + '/' + d.quarter,
+    //     y: parseFloat(d.count/1000)}
+    // });
 
     // KEY USER PARAMETERS
     // stroke
     // strokeWidth
 
+    componentDidMount = () => {
+        this.setState({
+            title: this.props.tableName,
+            dataArr: this.props.data,
+        })
+        console.log('after didmount ', this.state)
+    }
+    render() {
+        console.log('in render',this.state.dataArr)
+        const sampleData = [
+            { x: 0, y: 8 },
+            { x: 1, y: 5 },
+            { x: 2, y: 4 },
+            { x: 3, y: 9 },
+            { x: 4, y: 1 },
+            { x: 5, y: 7 },
+            { x: 6, y: 6 },
+            { x: 7, y: 3 },
+            { x: 8, y: 2 },
+            { x: 9, y: 0 },
+        ]
 
-    return (
-        <XYPlot
-            xType="ordinal"
-            width={1000}
-            height={500}>
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis title="Period of time(year and quarter)" />
-            <YAxis title="Number of pull requests (thousands)" />
-                <LineSeries
-                    data={dataArr}
-                    style={{
-                        stroke: 'red', 
-                        strokeWidth: 3,
-                        fill: 'none'
-                        }}                   
-                        
-                        />
-        </XYPlot>
-    );
+        return (
+            <div>
+                <XYPlot height={300} width={300}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis />
+                    <YAxis />
+                    {/* <LineSeries data={this.state.dataArr} style={{ fill: 'none' }} /> */}
+                    <LineSeries data={sampleData} style={{ fill: 'none' }} />
+                </XYPlot>
+                {this.state.dataArr}
+            </div>
+        )
+    }
 }
-
-export default LineGraph;
