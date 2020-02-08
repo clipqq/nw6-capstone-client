abf5e5ac592d838d68c430e04574c9f8ea183c57
Queen Shabazz <queennefertitishabazz@gmail.com>
2020-02-08 07:30:07 -0800

    passing 404 test

--- src/404Page/404.test.js
+++ src/404Page/404.test.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AddGraph from './404';
+import Four from './404';
 import renderer from 'react-test-renderer';
 
-describe('AddGraph Component', () => {
+describe('Four Component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(< AddGraph />, div);
+    ReactDOM.render(
+        < Four />
+    , div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('renders the UI as expected', () => {
     const tree = renderer
-      .create(<AddGraph/>)
+      .create(<Four/>)
       .toJSON();
     expect(tree).toMatchSnapshot();  
   });
--- src/AddGraph/AddGraph.test.js
+++ src/AddGraph/AddGraph.test.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AddGraph from './AddGraph';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 describe('AddGraph Component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(< AddGraph />, div);
+    ReactDOM.render(
+        <Route>
+          <AddGraph />
+        </Route>
+        
+    , div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('renders the UI as expected', () => {
     const tree = renderer
-      .create(<AddGraph/>)
+      .create(
+        <Route>
+          <AddGraph/>
+        </Route>)
       .toJSON();
     expect(tree).toMatchSnapshot();  
   });
